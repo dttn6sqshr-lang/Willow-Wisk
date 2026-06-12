dc0ab215c69c263a192ede633a902a09feed349e
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-12 10:25:21 -0400

    Update buttonEditor.js

--- dashboard/components/buttonEditor.js
+++ dashboard/components/buttonEditor.js
@@ -1,3 +1,15 @@
-export default function Component() {
-  return null;
+export default function ButtonEditor() {
+  return (
+    <div>
+      Add Button
+
+      Edit Label
+
+      Edit Custom ID
+
+      Edit Ticket Name
+
+      Delete Button
+    </div>
+  );
 }

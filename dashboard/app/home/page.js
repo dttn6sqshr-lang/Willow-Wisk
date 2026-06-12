f759a653418dad105b8302d3402dd89562e3eca3
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-12 10:09:13 -0400

    Update page.js

--- dashboard/app/home/page.js
+++ dashboard/app/home/page.js
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <h1>Coming Soon</h1>;
+import Sidebar from "../../components/sidebar";
+import Topbar from "../../components/topbar";
+import Card from "../../components/card";
+
+export default function Home() {
+  return (
+    <div>
+      <Sidebar />
+      <Topbar />
+
+      <Card title="Open Tickets" />
+      <Card title="Orders Completed" />
+      <Card title="Staff Activity" />
+      <Card title="Pending Verification" />
+    </div>
+  );
 }

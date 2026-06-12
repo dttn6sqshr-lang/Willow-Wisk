a304f5e77f40bc1b53638635aeef3a386055159b
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-12 10:07:20 -0400

    Update sidebar.js

--- dashboard/components/sidebar.js
+++ dashboard/components/sidebar.js
@@ -1,3 +1,23 @@
 export default function Sidebar() {
-  return "Sidebar";
+  return (
+    <div>
+      Dashboard
+
+      Home
+      Tickets
+      Verification
+      Welcome
+
+      Embeds
+      Orders
+      Automod
+      Moderation
+
+      Roles
+      Analytics
+      Backups
+      Logs
+      Settings
+    </div>
+  );
 }

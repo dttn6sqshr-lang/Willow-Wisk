748d4ca0defb72ba44ebd41e05b04d1438b08799
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-12 11:15:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Willow Wisk starting...");
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);

66da9b9fa7cb5f9b57158b0645942be834aea73a
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-12 12:10:36 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <h1>Willow Wisk Dashboard</h1>
-      <p>Dashboard is online</p>
+      <p>Dashboard is running correctly</p>
     </div>
   );
 }

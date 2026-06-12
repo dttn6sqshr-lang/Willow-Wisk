ddb373cc367f9dfe0b483ef13285a8a536574f05
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-12 10:15:00 -0400

    Update page.js

--- dashboard/app/tickets/page.js
+++ dashboard/app/tickets/page.js
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <h1>Coming Soon</h1>;
+import Sidebar from "../../components/sidebar";
+import Topbar from "../../components/topbar";
+
+export default function Tickets() {
+  return (
+    <div>
+      <Sidebar />
+      <Topbar />
+
+      Ticket Panel Builder
+
+      Button Builder
+
+      Ticket Categories
+
+      Ticket Permissions
+
+      Ticket Intro Messages
+
+      Ticket Analytics
+    </div>
+  );
 }

9d8b29fd5c80d4c0c1e1b02e7bccca51fa3a0923
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-18 18:36:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
-
-let activityLog = [];
-
-function addActivity(text){
-  activityLog.unshift({ text, time: Date.now() });
-  if(activityLog.length > 20) activityLog.pop();
-}
 
 client.once("ready", () => {
   console.log("Bot online");
-  addActivity("Bot started");
 });
-
-client.on("guildCreate", g => {
-  addActivity("Joined: " + g.name);
-});
-
-client.on("guildDelete", g => {
-  addActivity("Left: " + g.name);
-});
-
-/* expose for app.js later if needed */
-module.exports = { client, activityLog, addActivity };
 
 client.login(process.env.TOKEN);

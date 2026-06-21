6163d456673ce421667eb3f9ddd0294289fd978b
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-06-21 14:18:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -149,13 +149,7 @@ app.get("/api/stats", (req, res) => {
    GUILDS API (REAL BOT SERVERS)
 ====================== */
 app.get("/api/guilds", async (req, res) => {
-
-  if (!req.session.accessToken) {
-    return res.status(401).json([]);
-  }
-
   try {
-
     const guildRes = await axios.get(
       "https://discord.com/api/users/@me/guilds",
       {
@@ -165,14 +159,26 @@ app.get("/api/guilds", async (req, res) => {
       }
     );
 
-    const guilds = guildRes.data.map(g => ({
-  id: g.id,
-  name: g.name,
-  members: g.approximate_member_count || 0,
-  icon: g.icon
-    ? `https://cdn.discordapp.com/icons/${g.id}/${g.icon}.png?size=128`
-    : null
-}));
+    const guilds = guildRes.data;
+
+    const filtered = guilds.filter(g => {
+      const perms = BigInt(g.permissions);
+      const ADMIN = 0x8n;
+      const MANAGE_GUILD = 0x20n;
+
+      return (
+        (perms & ADMIN) === ADMIN ||
+        (perms & MANAGE_GUILD) === MANAGE_GUILD
+      );
+    });
+
+    res.json(filtered);
+
+  } catch (err) {
+    console.log(err);
+    res.json([]);
+  }
+});
 
   const perms = BigInt(g.permissions);
 
